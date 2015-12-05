275e31338cce0888ffaa859f44d8ee9b01488417
Matan Rosenberg <matan129@gmail.com>
2015-12-05 18:45:44 +0200

    Upadte build configuration

---
+++ build/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lpthread -lopencv_highgui -lopencv_videoio -lopencv_cudaimgproc -lopencv_cudafilters -lopencv_video -lopencv_imgproc -lopencv_cudaarithm -lopencv_core
+
